be84b11b98a47945f9c3b69fd1179bffb7f94ce6
Arazora // Taro <aman_a@aol.com>
2020-05-04 03:34:16 +0500

    Shell Execution (#8)
    * Shell Execution
    
    * Delete shell.py
    
    * Update chat_status.py
    
    * Fixed reporting ig
    
    * Revert "Fixed reporting ig"
    
    This reverts commit 83620454634d7ea0a42f61f82126751ff806ac1b.
    
    * Update bans.py
    
    * Update anime.py
    
    * wew
    
    * Update paser.py
    
    * paser fix lmao
    
    some issue with paser fix

--- tg_bot/modules/paser.py
+++ tg_bot/modules/paser.py
@@ -8,7 +8,7 @@ from tg_bot import dispatcher
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin, user_admin
 
-@user_admin
+
 @run_async
 def police(bot: Bot, update: Update):
     message = update.effective_message.reply_text('/police')
@@ -31,27 +31,67 @@ def police(bot: Bot, update: Update):
 
                 "🔵🔵🔵⬜️⬜️⬜️🔴🔴🔴\n🔵🔵🔵⬜️⬜️⬜️🔴🔴🔴\n🔵🔵🔵⬜️⬜️⬜️🔴🔴🔴",
 
-                "🔴🔴🔴⬜️⬜️⬜️🔵🔵🔵\n🔴🔴🔴⬜️⬜️⬜️🔵🔵🔵\n🔴🔴🔴⬜️⬜️⬜️🔵🔵🔵",
-
-                "🔵🔵🔵⬜️⬜️⬜️🔴🔴🔴\n🔵🔵🔵⬜️⬜️⬜️🔴🔴🔴\n🔵🔵🔵⬜️⬜️⬜️🔴🔴🔴",
-
-                "🔴🔴🔴⬜️⬜️⬜️🔵🔵🔵\n🔴🔴🔴⬜️⬜️⬜️🔵🔵🔵\n🔴🔴🔴⬜️⬜️⬜️🔵🔵🔵",
-
                 "*Police iz Here*"
     ]
     for i in animation_chars:
         message.edit_text(i)
         sleep(0.5)
 
+@run_async
+def moon(bot: Bot, update: Update):
+    message = update.effective_message.reply_text('/moon')
+    
+
+    animation_chars = [
+                "🌗🌘🌑🌒🌓🌔🌕🌖",
+
+                "🌘🌑🌒🌓🌔🌕🌖🌗",
+
+                "🌑🌒🌓🌔🌕🌖🌗🌘",
+
+                "🌒🌓🌔🌕🌖🌗🌘🌑",
+
+                "🌓🌔🌕🌖🌗🌘🌑🌒",    
+
+                "🌔🌕🌖🌗🌘🌑🌒🌓",
+
+                "🌕🌖🌗🌘🌑🌒🌓🌔",
+
+                "🌖🌗🌘🌑🌒🌓🌔🌕",
+
+                "🌗🌘🌑🌒🌓🌔🌕🌖",
+
+                "🌘🌑🌒🌓🌔🌕🌖🌗",
+
+                "🌑🌒🌓🌔🌕🌖🌗🌘",
+
+                "🌒🌓🌔🌕🌖🌗🌘🌑",
+
+                "🌓🌔🌕🌖🌗🌘🌑🌒",    
+
+                "🌔🌕🌖🌗🌘🌑🌒🌓",
+
+                "🌕🌖🌗🌘🌑🌒🌓🌔",
+
+                "🌖🌗🌘🌑🌒🌓🌔🌕",
+
+    ]
+    for i in animation_chars:
+        message.edit_text(i)
+        sleep(0.2)
+
 
 __help__ = """
- - /police : Try it.
+ - /police : *Sirens* Polize iz here
+ - /moon : Cycles all the phases of the moon emojis.
 """
 
 POLICE_HANDLER = DisableAbleCommandHandler(["police"], police)
+MOON_HANDLER = DisableAbleCommandHandler(["moon"], moon)
 
 dispatcher.add_handler(POLICE_HANDLER)
+dispatcher.add_handler(MOON_HANDLER)
 
-__mod_name__ = "Police!"
-__command_list__ = ["police"]
-__handlers__ = [POLICE_HANDLER]
+__mod_name__ = "Parser"
+__command_list__ = ["police", "moon"]
+__handlers__ = [POLICE_HANDLER, MOON_HANDLER]
