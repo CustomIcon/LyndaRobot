e2d6e4a98e6848de89e8736859e0a4710ca60765
AmaanAhmed <aman_a@aol.com>
2020-05-03 21:52:41 +0500

    Update shell.py

--- tg_bot/modules/shell.py
+++ tg_bot/modules/shell.py
@@ -4,7 +4,7 @@
 from tg_bot import dispatcher, LOGGER
 from telegram import Bot, Update
 from telegram.ext.dispatcher import run_async
-from tg_bot.modules.helper_funcs.chat_status import dev_user
+from tg_bot.modules.helper_funcs.chat_status import dev_plus
 from tg_bot.modules.helper_funcs.misc import sendMessage
 from telegram.ext import CommandHandler
 from subprocess import Popen, PIPE
