5faf61bece5aa63acd0e7aa02149d3fb382b370d
AmaanAhmed <aman_a@aol.com>
2020-05-03 21:48:25 +0500

    Better Wiki

--- tg_bot/modules/wiki.py
+++ tg_bot/modules/wiki.py
@@ -9,24 +9,37 @@ from tg_bot.modules.disable import DisableAbleCommandHandler
 
 @run_async
 def wiki(bot: Bot, update: Update):
-    message = update.effective_message
-    search_str = message.text.split(' ', 1)
-    if len(search_str) == 1:
-        searchterm = 'Usage: /wiki [search terms]'
-        message.reply_text(searchterm, parse_mode=ParseMode.MARKDOWN)
-    results = wikipedia.search(search_str)
-    if not results:
-        error = 'There were no results matching the query.'
-        message.reply_text(error, parse_mode=ParseMode.MARKDOWN)
+    msg = update.effective_message.reply_to_message if update.effective_message.reply_to_message else update.effective_message
+    res = ""
+    if msg == update.effective_message:
+        search = msg.text.split(" ", maxsplit=1)[1]
     else:
-        summary = wikipedia.summary(search_str)
-        reply_text = (summary)
-    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+        search = msg.text
+    try:
+        res = wikipedia.summary(search)
+    except DisambiguationError as e:
+        update.message.reply_text("Disambiguated pages found! Adjust your query accordingly.\n<i>{}</i>".format(e),
+        parse_mode=ParseMode.HTML)
+    except PageError as e:
+        update.message.reply_text("<code>{}</code>".format(e), parse_mode=ParseMode.HTML)
+    if res:
+        result = f"<b>{search}</b>\n\n"
+        result += f"<i>{res}</i>\n"
+        result += f"""<a href="https://en.wikipedia.org/wiki/{search.replace(" ", "%20")}">Read more...</a>"""
+        if len(result) > 4000:
+            with open("result.txt", 'w') as f:
+                f.write(f"{result}\n\nUwU OwO OmO UmU")
+            with open("result.txt", 'rb') as f:
+                bot.send_document(document=f, filename=f.name,
+                    reply_to_message_id=update.message.message_id, chat_id=update.effective_chat.id,
+                    parse_mode=ParseMode.HTML)
+        else:
+            update.message.reply_text(result, parse_mode=ParseMode.HTML)
 
 
 __help__ = """
- - /wiki oogabooga
- - /wikipedia oogabooga
+ - /wiki - Search terms /wiki <query>
+ - /wikipedia - Same as /wiki
 """
 
 WIKI_HANDLER = DisableAbleCommandHandler(["wiki", "wikipedia"], wiki)
