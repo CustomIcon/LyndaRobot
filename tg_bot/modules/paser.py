3ae08666fb4cf3451899592556e76900522eb06d
AmaanAhmed <aman_a@aol.com>
2020-05-04 03:14:59 +0500

    Update paser.py

--- tg_bot/modules/paser.py
+++ tg_bot/modules/paser.py
@@ -8,7 +8,7 @@ from tg_bot import dispatcher
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin, user_admin
 
-@user_admin
+
 @run_async
 def police(bot: Bot, update: Update):
     message = update.effective_message.reply_text('/police')
@@ -43,15 +43,61 @@ def police(bot: Bot, update: Update):
         message.edit_text(i)
         sleep(0.5)
 
+@run_async
+def moon(bot: Bot, update: Update):
+    message = update.effective_message.reply_text('/moon')
+    
+
+    animation_chars = [
+                "🌗🌘🌑🌒🌓🌔🌕🌖",
+
+                "🌘🌑🌒🌓🌔🌕🌖🌗",
+
+                "🌑🌒🌓🌔🌕🌖🌗🌘",
+
+                "🌒🌓🌔🌕🌖🌗🌘🌑",
+
+                "🌓🌔🌕🌖🌗🌘🌑🌒",    
+
+                "🌔🌕🌖🌗🌘🌑🌒🌓",
+
+                "🌕🌖🌗🌘🌑🌒🌓🌔",
+
+                "🌖🌗🌘🌑🌒🌓🌔🌕",
+
+                "🌗🌘🌑🌒🌓🌔🌕🌖",
+
+                "🌘🌑🌒🌓🌔🌕🌖🌗",
+
+                "🌑🌒🌓🌔🌕🌖🌗🌘",
+
+                "🌒🌓🌔🌕🌖🌗🌘🌑",
+
+                "🌓🌔🌕🌖🌗🌘🌑🌒",    
+
+                "🌔🌕🌖🌗🌘🌑🌒🌓",
+
+                "🌕🌖🌗🌘🌑🌒🌓🌔",
+
+                "🌖🌗🌘🌑🌒🌓🌔🌕",
+
+    ]
+    for i in animation_chars:
+        message.edit_text(i)
+        sleep(0.1)
+
 
 __help__ = """
- - /police : Try it.
+ - /police : *Sirens* Polize iz here
+ - /moon : Cycles all the phases of the moon emojis.
 """
 
 POLICE_HANDLER = DisableAbleCommandHandler(["police"], police)
+MOON_HANDLER = DisableAbleCommandHandler(["moon"], moon)
 
 dispatcher.add_handler(POLICE_HANDLER)
+dispatcher.add_handler(MOON_HANDLER)
 
-__mod_name__ = "Police!"
-__command_list__ = ["police"]
-__handlers__ = [POLICE_HANDLER]
+__mod_name__ = "Parser"
+__command_list__ = ["police", "moon"]
+__handlers__ = [POLICE_HANDLER, MOON_HANDLER]
