8188cebb8a28ec7098eecf3661b2dda020344be9
AzaroTaro <amanhmedzahir@hotmail.com>
2020-05-05 00:06:30 +0500

    test

--- tg_bot/modules/wiki.py
+++ tg_bot/modules/wiki.py
@@ -1,50 +1,30 @@
+from telegram import ChatAction
+import html
+import urllib.request
+import re
+import json
+from typing import Optional, List
+import time
+import urllib
+from urllib.request import urlopen, urlretrieve
+from urllib.parse import quote_plus, urlencode
 import requests
-import datetime
-from telegram import Update, Bot, ParseMode
-from telegram.ext import run_async
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from telegram import ParseMode
+from telegram.ext import CommandHandler, run_async, Filters
+from haruka import dispatcher
+from haruka.__main__ import STATS, USER_INFO
+from haruka.modules.disable import DisableAbleCommandHandler
 import wikipedia
-from wikipedia.exceptions import DisambiguationError, PageError
 
-from tg_bot import dispatcher
-from tg_bot.modules.disable import DisableAbleCommandHandler
-
-@run_async
-def wiki(bot: Bot, update: Update):
-    msg = update.effective_message.reply_to_message if update.effective_message.reply_to_message else update.effective_message
-    res = ""
-    if msg == update.effective_message:
-        search = msg.text.split(" ", maxsplit=1)[1]
-    else:
-        search = msg.text
-    try:
-        res = wikipedia.summary(search)
-    except DisambiguationError as e:
-        update.message.reply_text("Disambiguated pages found! Adjust your query accordingly.\n<i>{}</i>".format(e),
-        parse_mode=ParseMode.HTML)
-    except PageError as e:
-        update.message.reply_text("<code>{}</code>".format(e), parse_mode=ParseMode.HTML)
-    if res:
-        result = f"<b>{search}</b>\n\n"
-        result += f"<i>{res}</i>\n"
-        result += f"""<a href="https://en.wikipedia.org/wiki/{search.replace(" ", "%20")}">Read more...</a>"""
-        if len(result) > 4000:
-            with open("result.html", 'w') as f:
-                f.write(f"{result}\n")
-            with open("result.html", 'rb') as f:
-                bot.send_document(document=f, filename=f.name,
-                    reply_to_message_id=update.message.message_id, chat_id=update.effective_chat.id,
-                    parse_mode=ParseMode.HTML)
-        else:
-            update.message.reply_text(result, parse_mode=ParseMode.HTML)
-
-
+def wiki(bot: Bot, update: Update, args):
+    reply = " ".join(args)
+    summary = '{} <a href="{}">more</a>'
+    update.message.reply_text(summary.format(wikipedia.summary(reply, sentences=3), wikipedia.page(reply).url), pasrse_mode=HTML)
+		
 __help__ = """
- - /wiki - Search Query with /wiki <query>
- - /wikipedia - Same as /wiki
+ - /wiki text: Returns search from wikipedia for the input text
 """
-
-WIKI_HANDLER = DisableAbleCommandHandler(["wiki", "wikipedia"], wiki)
-
-dispatcher.add_handler(WIKI_HANDLER)
-
 __mod_name__ = "Wikipedia"
+WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki, pass_args=True)
+dispatcher.add_handler(WIKI_HANDLER)
