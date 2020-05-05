9b86e53a2a93641382a48a4f13e6a16ddc1ffd39
AzaroTaro <amanhmedzahir@hotmail.com>
2020-05-06 04:51:11 +0500

    shell added nsfw removed

--- tg_bot/modules/nsfw.py
+++ tg_bot/modules/nsfw.py
@@ -1,63 +1,63 @@
-from telegram import ChatAction
-import html
-import urllib.request
-import re
-import json
-from typing import Optional, List
-import time
-import urllib
-from urllib.request import urlopen, urlretrieve
-from urllib.parse import quote_plus, urlencode
-import requests
-from telegram import Message, Chat, Update, Bot, MessageEntity
-from telegram import ParseMode
-from telegram.ext import CommandHandler, run_async, Filters
-from tg_bot import dispatcher, updater
-from tg_bot.modules.disable import DisableAbleCommandHandler
-from tg_bot.modules.helper_funcs.chat_status import is_user_admin, user_admin
+# from telegram import ChatAction
+# import html
+# import urllib.request
+# import re
+# import json
+# from typing import Optional, List
+# import time
+# import urllib
+# from urllib.request import urlopen, urlretrieve
+# from urllib.parse import quote_plus, urlencode
+# import requests
+# from telegram import Message, Chat, Update, Bot, MessageEntity
+# from telegram import ParseMode
+# from telegram.ext import CommandHandler, run_async, Filters
+# from tg_bot import dispatcher, updater
+# from tg_bot.modules.disable import DisableAbleCommandHandler
+# from tg_bot.modules.helper_funcs.chat_status import is_user_admin, user_admin
 
-def hentai(bot: Bot, update: Update):
-    msg = update.effective_message
-    nsfw = requests.get("https://api.computerfreaker.cf/v1/hentai").json()
-    url = nsfw.get("url")
-    # do shit with url if you want to
-    if not url:
-        msg.reply_text("No URL was received from the API!")
-        return
-    msg.reply_photo(url)
+# def hentai(bot: Bot, update: Update):
+#     msg = update.effective_message
+#     nsfw = requests.get("https://api.computerfreaker.cf/v1/hentai").json()
+#     url = nsfw.get("url")
+#     # do shit with url if you want to
+#     if not url:
+#         msg.reply_text("No URL was received from the API!")
+#         return
+#     msg.reply_photo(url)
 
-def neko(bot: Bot, update: Update):
-    msg = update.effective_message
-    nsfw = requests.get("https://api.computerfreaker.cf/v1/nsfwneko").json()
-    url = nsfw.get("url")
-    # do shit with url if you want to
-    if not url:
-        msg.reply_text("No URL was received from the API!")
-        return
-    msg.reply_photo(url)
+# def neko(bot: Bot, update: Update):
+#     msg = update.effective_message
+#     nsfw = requests.get("https://api.computerfreaker.cf/v1/nsfwneko").json()
+#     url = nsfw.get("url")
+#     # do shit with url if you want to
+#     if not url:
+#         msg.reply_text("No URL was received from the API!")
+#         return
+#     msg.reply_photo(url)
 
-def dva(bot: Bot, update: Update):
-    msg = update.effective_message
-    nsfw = requests.get("https://api.computerfreaker.cf/v1/dva").json()
-    url = nsfw.get("url")
-    # do shit with url if you want to
-    if not url:
-        msg.reply_text("No URL was received from the API!")
-        return
-    msg.reply_photo(url)
+# def dva(bot: Bot, update: Update):
+#     msg = update.effective_message
+#     nsfw = requests.get("https://api.computerfreaker.cf/v1/dva").json()
+#     url = nsfw.get("url")
+#     # do shit with url if you want to
+#     if not url:
+#         msg.reply_text("No URL was received from the API!")
+#         return
+#     msg.reply_photo(url)
 
-__help__ = """
- - /hentai: Sends Random Hentai Images.
- - /neko: Sends Random neko source Images.
- - /dva: Sends Random D.VA source Images.
-"""
+# __help__ = """
+#  - /hentai: Sends Random Hentai Images.
+#  - /neko: Sends Random neko source Images.
+#  - /dva: Sends Random D.VA source Images.
+# """
 
-__mod_name__ = "NSFW"
-HENTAI_HANDLER = DisableAbleCommandHandler("hentai", hentai)
-NEKO_HANDLER = DisableAbleCommandHandler("neko", neko)
-DVA_HANDLER = DisableAbleCommandHandler("dva", dva)
+# __mod_name__ = "NSFW"
+# HENTAI_HANDLER = DisableAbleCommandHandler("hentai", hentai)
+# NEKO_HANDLER = DisableAbleCommandHandler("neko", neko)
+# DVA_HANDLER = DisableAbleCommandHandler("dva", dva)
 
-dispatcher.add_handler(HENTAI_HANDLER)
-dispatcher.add_handler(NEKO_HANDLER)
-dispatcher.add_handler(DVA_HANDLER)
+# dispatcher.add_handler(HENTAI_HANDLER)
+# dispatcher.add_handler(NEKO_HANDLER)
+# dispatcher.add_handler(DVA_HANDLER)
 
