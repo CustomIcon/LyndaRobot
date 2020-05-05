afa7c890084c1853dfbfd6c1b9f03cc06bef4f22
AzaroTaro <amanhmedzahir@hotmail.com>
2020-05-05 08:50:44 +0500

    dont ask

--- tg_bot/modules/nsfw.py
+++ tg_bot/modules/nsfw.py
@@ -36,17 +36,30 @@ def hentai(bot: Bot, update: Update):
         return
     msg.reply_photo(url)
 
+def neko(bot: Bot, update: Update):
+    msg = update.effective_message
+    nsfw = requests.get("https://api.computerfreaker.cf/v1/nsfwneko).json()
+    url = nsfw.get("url")
+    # do shit with url if you want to
+    if not url:
+        msg.reply_text("No URL was received from the API!")
+        return
+    msg.reply_photo(url)
+
 __help__ = """
  - /boobs: Sends Random tiddie pic.
  - /butts: Sends Random bun pic.
  - /hentai: Sends Random Hentai Images.
+ - /neko: Sends Random nekoNSFW Images.
 """
 
 __mod_name__ = "NSFW"
 BUTTS_HANDLER = DisableAbleCommandHandler("butts", butts)
 BOOBS_HANDLER = DisableAbleCommandHandler("boobs", boobs)
 HENTAI_HANDLER = DisableAbleCommandHandler("hentai", hentai)
+NEKO_HANDLER = DisableAbleCommandHandler("neko", neko)
 
 dispatcher.add_handler(BUTTS_HANDLER)
 dispatcher.add_handler(BOOBS_HANDLER)
 dispatcher.add_handler(HENTAI_HANDLER)
+dispatcher.add_handler(NEKO_HANDLER)
