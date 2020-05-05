205b88d70cd204ac199783ed2e913ee59450d129
AzaroTaro <amanhmedzahir@hotmail.com>
2020-05-05 21:01:52 +0500

    removed tiddies and buns

--- tg_bot/modules/nsfw.py
+++ tg_bot/modules/nsfw.py
@@ -16,16 +16,6 @@ from tg_bot import dispatcher, updater
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin, user_admin
 
-def butts(bot: Bot, update: Update):
-    nsfw = requests.get('http://api.obutts.ru/noise/1').json()[0]["preview"]
-    final = "http://media.obutts.ru/{}".format(nsfw)
-    update.message.reply_photo(final)
-
-def boobs(bot: Bot, update: Update):
-    nsfw = requests.get('http://api.oboobs.ru/noise/1').json()[0]["preview"]
-    final = "http://media.oboobs.ru/{}".format(nsfw)
-    update.message.reply_photo(final)
-
 def hentai(bot: Bot, update: Update):
     msg = update.effective_message
     nsfw = requests.get("https://api.computerfreaker.cf/v1/hentai").json()
@@ -57,22 +47,16 @@ def dva(bot: Bot, update: Update):
     msg.reply_photo(url)
 
 __help__ = """
- - /boobs: Sends Random tiddie pic.
- - /butts: Sends Random bun pic.
  - /hentai: Sends Random Hentai Images.
  - /neko: Sends Random neko source Images.
  - /dva: Sends Random D.VA source Images.
 """
 
 __mod_name__ = "NSFW"
-BUTTS_HANDLER = DisableAbleCommandHandler("butts", butts)
-BOOBS_HANDLER = DisableAbleCommandHandler("boobs", boobs)
 HENTAI_HANDLER = DisableAbleCommandHandler("hentai", hentai)
 NEKO_HANDLER = DisableAbleCommandHandler("neko", neko)
 DVA_HANDLER = DisableAbleCommandHandler("dva", dva)
 
-dispatcher.add_handler(BUTTS_HANDLER)
-dispatcher.add_handler(BOOBS_HANDLER)
 dispatcher.add_handler(HENTAI_HANDLER)
 dispatcher.add_handler(NEKO_HANDLER)
 dispatcher.add_handler(DVA_HANDLER)
