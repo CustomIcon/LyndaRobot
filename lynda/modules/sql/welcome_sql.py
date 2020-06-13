c81fc970e10181cd441da6486c1c9a36379440d9
AzaroTaro <amanhmedzahir@hotmail.com>
2020-06-13 12:33:49 +0500

    [PYL-W1401]
    Fixed anomalous backslashes

--- lynda/modules/sql/welcome_sql.py
+++ lynda/modules/sql/welcome_sql.py
@@ -35,7 +35,7 @@ DEFAULT_WELCOME_MESSAGES = [
     "{first} just showed up. Hold my beer.",
     "Challenger approaching! {first} has appeared!",
     "It's a bird! It's a plane! Nevermind, it's just {first}.",
-    "It's {first}! Praise the sun! \o/",
+    "It's {first}! Praise the sun!",
     "Never gonna give {first} up. Never gonna let {first} down.",
     "Ha! {first} has joined! You activated my trap card!",
     "Cheers, love! {first}'s here!",
