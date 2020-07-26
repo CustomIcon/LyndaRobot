cdf48135fc0e61b8ce514a45b4a4261ca1e9383e
pokurt <aman@virtual.mv>
2020-07-26 15:38:14 +0500

    Merge pull request #34 from Dank-del/master
    idk
    
    Co-Authored-By: Skadi <63096193+dank-del@users.noreply.github.com>

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -1,6 +1,7 @@
 import importlib
 import re
 from typing import Optional, List
+from sys import argv
 
 from telegram import Bot, Update, ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.error import Unauthorized, BadRequest, TimedOut, NetworkError, ChatMigrated, TelegramError
@@ -9,7 +10,7 @@ from telegram.ext.dispatcher import run_async, DispatcherHandlerStop
 from telegram.utils.helpers import escape_markdown
 
 from tg_bot import dispatcher, updater, TOKEN, WEBHOOK, OWNER_ID, DONATION_LINK, CERT_PATH, PORT, URL, LOGGER, \
-    ALLOW_EXCL
+    ALLOW_EXCL, telethn
 # needed to dynamically load modules
 # NOTE: Module order is not guaranteed, specify that in the config file!
 from tg_bot.modules import ALL_MODULES
@@ -29,24 +30,16 @@ The support group chat is at @YorktownEagleUnion
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a part of [Eagle Union](https://telegra.ph/file/095d7e696096e21b06447.jpg)
-
+I'm a part of *Eagle Union*
 Have a look at the following for an idea of some of \
 the things I can help you with.
-
-AI and blacklist module from [TheRealPhoenixBot](https://github.com/rsktg/TheRealPhoenixBot.git)
-Source code from [SaitamaRobot](https://github.com/AnimeKaizoku/SaitamaRobot)
-
 *Main* commands available:
  - /start: start the bot
  - /help: PM's you this message.
  - /help <module name>: PM's you info about that module.
- - /donate: information about how to donate!
  - /settings:
    - in PM: will send you your settings for all supported modules.
    - in a group: will redirect you to pm, with all that chat's settings.
-
-
 {}
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
@@ -54,10 +47,7 @@ And the following:
 
 KIGYO_IMG = "https://telegra.ph/file/e5100e06c03767af80023.jpg"
 
-DONATE_STRING = """Heya, glad to hear you want to donate!
-Kigyō is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
-You can donate to the original writer of the Base code, Paul
-There are two ways of supporting him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+DONATE_STRING = """I'm free for everyone!! """
 
 IMPORTED = {}
 MIGRATEABLE = []
@@ -451,10 +441,14 @@ def main():
     else:
         LOGGER.info("Using long polling.")
         updater.start_polling(timeout=15, read_latency=4)
-
+    if len(argv) not in (1, 3, 4):
+        telethn.disconnect()
+    else:
+        telethn.run_until_disconnected()
     updater.idle()
 
 
 if __name__ == '__main__':
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
+    telethn.start(bot_token=TOKEN)
     main()
