e016e354d5cdc0cdee04faec16c31267e6082ee4
pokurt <aman@virtual.mv>
2020-08-03 16:29:11 +0500

    fixed nonetype error

--- lynda/modules/quote.py
+++ lynda/modules/quote.py
@@ -383,7 +383,7 @@ async def _(event):
         await event.client.get_entity(reply.forward.sender) if reply.fwd_from
         else reply.sender)
     res, canvas = await process(msg, user, event.client, reply, repliedreply)
-    if msg is None:
+    if not msg:
         await event.reply('Reply to message to make a sticker')
         return
     if not res:
