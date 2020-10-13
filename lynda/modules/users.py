7410aca9cef1007f2992a35db66a2e9f7897acfd
ポキ <aman_a@aol.com>
2020-10-13 19:02:54 +0500

    fixed issues in users.py
    Regarding to sql errors this needs to be done

--- lynda/modules/users.py
+++ lynda/modules/users.py
@@ -1,16 +1,23 @@
+
 from io import BytesIO
 from time import sleep
 
-from telegram import Update, TelegramError
-from telegram.error import BadRequest
-from telegram.ext import CommandHandler, MessageHandler, Filters, run_async, CallbackContext
-
 import lynda.modules.sql.users_sql as sql
-
-from lynda import dispatcher, OWNER_ID, LOGGER, DEV_USERS
-from lynda.modules.helper_funcs.chat_status import sudo_plus, dev_plus
+from lynda import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from lynda.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from lynda.modules.sql.users_sql import get_all_users
+from telegram import TelegramError, Update
+from telegram.error import BadRequest
+from telegram.ext import (
+    CallbackContext,
+    CommandHandler,
+    Filters,
+    MessageHandler,
+    run_async
+)
 
 USERS_GROUP = 4
+CHAT_GROUP = 5
 DEV_AND_MORE = DEV_USERS.append(int(OWNER_ID))
 
 
@@ -38,75 +45,121 @@ def get_user_id(username):
                     return userdat.id
 
             except BadRequest as excp:
-                if excp.message != 'Chat not found':
+                if excp.message == 'Chat not found':
+                    pass
+                else:
                     LOGGER.exception("Error extracting user ID")
 
     return None
+
+
 @run_async
 @dev_plus
 def broadcast(update: Update, context: CallbackContext):
-    bot = context.bot
-
     to_send = update.effective_message.text.split(None, 1)
 
     if len(to_send) >= 2:
+        to_group = False
+        to_user = False
+        if to_send[0] == '/broadcastgroups':
+            to_group = True
+        if to_send[0] == '/broadcastusers':
+            to_user = True
+        else:
+            to_group = to_user = True
         chats = sql.get_all_chats() or []
+        users = get_all_users()
         failed = 0
-        for chat in chats:
-            try:
-                bot.sendMessage(int(chat.chat_id), to_send[1])
-                sleep(0.1)
-            except TelegramError:
-                failed += 1
-                LOGGER.warning(
-                    "Couldn't send broadcast to %s, group name %s", str(
-                        chat.chat_id), str(
-                        chat.chat_name))
-
+        failed_user = 0
+        if to_group:
+            for chat in chats:
+                try:
+                    context.bot.sendMessage(
+                        int(chat.chat_id),
+                        to_send[1],
+                        parse_mode="MARKDOWN",
+                        disable_web_page_preview=True)
+                    sleep(0.1)
+                except TelegramError:
+                    failed += 1
+        if to_user:
+            for user in users:
+                try:
+                    context.bot.sendMessage(
+                        int(user.user_id),
+                        to_send[1],
+                        parse_mode="MARKDOWN",
+                        disable_web_page_preview=True)
+                    sleep(0.1)
+                except TelegramError:
+                    failed_user += 1
         update.effective_message.reply_text(
-            f"Broadcast complete. {failed} groups failed to receive the message, probably due to being kicked.")
+            f"Broadcast complete.\nGroups failed: {failed}.\nUsers failed: {failed_user}."
+        )
 
 
 @run_async
-def log_user(update: Update, _):
+def log_user(update: Update, context: CallbackContext):
     chat = update.effective_chat
     msg = update.effective_message
 
-    sql.update_user(msg.from_user.id,
-                    msg.from_user.username,
-                    chat.id,
+    sql.update_user(msg.from_user.id, msg.from_user.username, chat.id,
                     chat.title)
 
     if msg.reply_to_message:
         sql.update_user(msg.reply_to_message.from_user.id,
-                        msg.reply_to_message.from_user.username,
-                        chat.id,
+                        msg.reply_to_message.from_user.username, chat.id,
                         chat.title)
 
     if msg.forward_from:
-        sql.update_user(msg.forward_from.id,
-                        msg.forward_from.username)
+        sql.update_user(msg.forward_from.id, msg.forward_from.username)
 
 
 @run_async
 @sudo_plus
-def chats(update: Update, _):
-
+def chats(update: Update, context: CallbackContext):
+    bot = context.bot
     all_chats = sql.get_all_chats() or []
-    chatfile = 'List of chats.\n'
+    chatfile = 'List of chats.\n0. Chat name | Chat ID | Members count\n'
+    P = 1
     for chat in all_chats:
-        chatfile += f"{chat.chat_name} - ({chat.chat_id})\n"
+        try:
+            curr_chat = bot.getChat(chat.chat_id)
+            bot_member = curr_chat.get_member(bot.id)
+            chat_members = curr_chat.get_members_count(bot.id)
+            chatfile += "{}. {} | {} | {}\n".format(P, chat.chat_name,
+                                                    chat.chat_id, chat_members)
+            P = P + 1
+        except:
+            pass
 
     with BytesIO(str.encode(chatfile)) as output:
         output.name = "chatlist.txt"
         update.effective_message.reply_document(
             document=output,
             filename="chatlist.txt",
-            caption="Here is the list of chats in my Hit List.")
+            caption="Here is the list of chats in my database.")
+
+
+@run_async
+def chat_checker(update: Update, context: CallbackContext):
+    bot = context.bot
+    if update.effective_message.chat.get_member(
+            bot.id).can_send_messages is False:
+        bot.leaveChat(update.effective_message.chat.id)
+
+
+def __user_info__(user_id):
+    if user_id in [777000, 1087968824]:
+        return """Groups count: <code>???</code>"""
+    if user_id == dispatcher.bot.id:
+        return """Groups count: <code>???</code>"""
+    num_chats = sql.get_user_num_chats(user_id)
+    return f"""Groups count: <code>{num_chats}</code> """
 
 
 def __stats__():
-    return f"{sql.num_users()} users, across {sql.num_chats()} chats"
+    return f"• {sql.num_users()} users, across {sql.num_chats()} chats"
 
 
 def __migrate__(old_chat_id, new_chat_id):
@@ -115,17 +168,17 @@ def __migrate__(old_chat_id, new_chat_id):
 
 __help__ = ""  # no help string
 
-BROADCAST_HANDLER = CommandHandler("broadcast", broadcast)
+BROADCAST_HANDLER = CommandHandler(
+    ["broadcastall", "broadcastusers", "broadcastgroups"], broadcast)
 USER_HANDLER = MessageHandler(Filters.all & Filters.group, log_user)
+CHAT_CHECKER_HANDLER = MessageHandler(Filters.all & Filters.group, chat_checker)
 CHATLIST_HANDLER = CommandHandler("chatlist", chats)
 
 dispatcher.add_handler(USER_HANDLER, USERS_GROUP)
 dispatcher.add_handler(BROADCAST_HANDLER)
 dispatcher.add_handler(CHATLIST_HANDLER)
+dispatcher.add_handler(CHAT_CHECKER_HANDLER, CHAT_GROUP)
 
 __mod_name__ = "Users"
-__handlers__ = [
-    (USER_HANDLER,
-     USERS_GROUP),
-    BROADCAST_HANDLER,
-    CHATLIST_HANDLER]
+__handlers__ = [(USER_HANDLER, USERS_GROUP), BROADCAST_HANDLER,
+                CHATLIST_HANDLER]
